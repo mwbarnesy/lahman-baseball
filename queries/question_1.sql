d3d461102fc987f7d2be3bd3b51de6dd86ecbf39
mwbarnesy <mwbarnesy@gmail.com>
2020-01-30 19:32:16 -0600

    Answered question 1!

--- queries/question_1.sql
+++ queries/question_1.sql
@@ -1,26 +1,28 @@
 /*
     QUESTION ::
-        ...
+        What range of years does the provided database cover?
 
     SOURCES ::
-        * ...
+        * Teams table
 
     DIMENSIONS ::
-        * ...
+        * yearid
 
     FACTS ::
-        * ...
+        * The minimum year is 1871
 
     FILTERS ::
-        * ...
+        * GROUP BY yearid, ORDER BY yearid ASC
 
     DESCRIPTION ::
         ...
 
     ANSWER ::
-        ...
+        1871
 
 */
 
-SELECT *
-FROM ...;
+SELECT MIN(yearid)
+FROM teams
+GROUP BY yearid
+ORDER BY yearid;
