903c85b4c77e4c48db209ca530101376f4098de0
mwbarnesy <mwbarnesy@gmail.com>
2020-02-01 13:49:42 -0600

    Started querying the salaries table.

--- queries/question_3.sql
+++ queries/question_3.sql
@@ -29,43 +29,54 @@
 */
 
 -- PART ONE: Find all players in the database who played at Vanderbilt University.
+-- PART TWO: List all players, first and last name, and the total salary earned in the major leagues.
 
--- Create a CTE that SELECTS schoolid and schoolname WHERE schoolname = 'Vanderbilt University'.
--- Create a CTE that returns the playerid, and the total monies earned from the salaries table.
 WITH 
-vandy AS
+	vandy AS -- Create a CTE that SELECTs schoolid and schoolname WHERE schoolname = 'Vanderbilt University'.
 	(
-	SELECT 
-		schoolid,
-		schoolname
-	FROM schools
-	WHERE schoolname = 'Vanderbilt University'
+		SELECT 
+			schoolid,
+			schoolname
+		FROM schools
+		WHERE schoolname = 'Vanderbilt University'
 	) -- End of vandy CTE
-,
-salary AS 
+	-- , ==========> DON'T FORGET TO COMMENT IN/OUT COMMAS!!! <==========
+/*	
+	salary AS -- Create a CTE that returns the playerid, and the total monies earned from the salaries table.
 	(
-	SELECT 
-		playerid, -- This column was only included to test the salary CTE.
-		SUM(salary) AS total_monies
-	FROM salaries
-	GROUP BY playerid -- Must GROUP BY a non aggregated column.
-	-- ORDER BY total_monies DESC -- This allows me to see the highest earner at a glance.
-	-- The previous two lines were commented out because I wanted to test the query to make sure it worked. 
+		SELECT 
+			playerid, -- This column was only included to test the salary CTE.
+			SUM(salary) AS total_monies
+		FROM salaries
+		GROUP BY playerid -- Must GROUP BY a non aggregated column.
+		-- ORDER BY total_monies DESC -- This allows me to see the highest earner at a glance.
+		-- The previous two lines were commented out because I wanted to test the query to make sure it worked. 
 	) -- End of salary CTE
+*/
+
 -- End of WITH clause
 
 -- In the outer query, select the schoolname from vandy, and playerid from collegeplaying.
-SELECT vandy.schoolname, p.namefirst, p.namelast
+SELECT 
+	-- vandy.schoolname, -- I only needed this to be sure that all returned players went to Vanderbilt.
+	p.namefirst, 
+	p.namelast
 FROM vandy
--- Create an INNER JOIN to join vandy to collegeplaying ON schoolid
+-- Create an INNER JOIN to join vandy to collegeplaying ON schoolid.
 INNER JOIN collegeplaying AS c
 ON vandy.schoolid = c.schoolid
--- Create another INNER JOIN to join people to collegeplaying ON playerid
+-- Create another INNER JOIN to join people to collegeplaying ON playerid.
 INNER JOIN people AS p
 ON p.playerid = c.playerid
+-- GROUP BY last name gets rid of duplicate rows.
 GROUP BY p.namelast, p.namefirst, vandy.schoolname
 ;
 
+-- Let's take a look at the salaries table with the highest salary at the top.
+SELECT playerid, MAX(salary) AS salary
+FROM salaries
+GROUP BY playerid
+ORDER BY salary DESC;
 
 
 
