cb2871ee3e397d48d408f514f248430c3219231f
mwbarnesy <mwbarnesy@gmail.com>
2020-02-04 21:19:20 -0600

    Started CASE WHEN.

--- queries/question_4.sql
+++ queries/question_4.sql
@@ -1,13 +1,16 @@
 /*
     QUESTION ::
-        ...
+        Using the fielding table, group players into three groups based on their position: label players 
+		with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and 
+		those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these 
+		three groups in 2016.
 
     SOURCES ::
-        * ...
+        People table, fielding table
 
     DIMENSIONS ::
-        * ...
-
+        Playerid, 
+		
     FACTS ::
         * ...
 
@@ -22,5 +25,23 @@
 
 */
 
-SELECT *
-FROM ...;
+SELECT 
+	p.playerid,
+	p.namefirst,
+	p.namelast, 
+	f.pos,
+		CASE 
+			WHEN pos = '1B' THEN 'Infield'
+			WHEN pos = '2B' THEN 'Infield'
+			WHEN pos = 'SS' THEN 'Infield'
+			WHEN pos = '3B' THEN 'Infield'
+			WHEN pos = 'OF' THEN 'Outfield'
+			WHEN pos = 'P' THEN 'Battery'
+			WHEN pos = 'C' THEN 'Battery'
+		END AS position_groups,
+	COUNT(position_groups)
+FROM people AS p
+	INNER JOIN fielding AS f
+	ON p.playerid = f.playerid
+GROUP BY position_groups
+;
