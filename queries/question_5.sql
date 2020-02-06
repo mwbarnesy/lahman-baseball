9b47284aa6d2b133ac3a091dcb231f7f181d5ec7
mwbarnesy <mwbarnesy@gmail.com>
2020-02-06 12:51:47 -0600

    Initial commit.

---
+++ queries/question_5.sql
@@ -0,0 +1,26 @@
+/*
+    QUESTION ::
+        Find the average number of strikeouts per game by decade since 1920. Round the numbers you report 
+		to 2 decimal places. Do the same for home runs per game. Do you see any trends?
+    SOURCES ::
+        * ...
+
+    DIMENSIONS ::
+        * ...
+
+    FACTS ::
+        * ...
+
+    FILTERS ::
+        * ...
+
+    DESCRIPTION ::
+        ...
+
+    ANSWER ::
+        ...
+
+*/
+
+SELECT *
+FROM ...;
