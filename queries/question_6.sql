a9b30da525fcf1b0f5eff426796a38c38517b76a
mwbarnesy <mwbarnesy@gmail.com>
2020-02-07 12:37:06 -0600

    Found the attempted stolen bases (SB + CS) and SUMmed them.

--- queries/question_6.sql
+++ queries/question_6.sql
@@ -1,18 +1,21 @@
 /*
     QUESTION ::
-        ...
+        Find the player who had the most success stealing bases in 2016, where success is measured 
+		as the percentage of stolen base attempts which are successful. (A stolen base attempt results 
+		either in a stolen base or being caught stealing.) Consider only players who attempted at least 
+		20 stolen bases.
 
     SOURCES ::
-        * ...
+        Batting table
 
     DIMENSIONS ::
-        * ...
+        Stolen base / caught stealing
 
     FACTS ::
-        * ...
+        Percentage of stolen base attempts which are successful, attempts are SUM(stolen base) + SUM(caught stealing)
 
     FILTERS ::
-        * ...
+        Year = 2016, players who ATTEMPTED to steal >= 20 
 
     DESCRIPTION ::
         ...
@@ -22,5 +25,10 @@
 
 */
 
-SELECT *
-FROM ...;
+SELECT
+	playerid,
+	SUM(SB) + SUM(CS) AS sb_attempts
+FROM batting
+WHERE yearid = 2016
+GROUP BY playerid
+;
