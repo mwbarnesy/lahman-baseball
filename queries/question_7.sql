84c5da5a0e5ddb6f767110d504baaef0a47b27da
mwbarnesy <mwbarnesy@gmail.com>
2020-02-07 23:07:08 -0600

    Intial commit.

---
+++ queries/question_7.sql
@@ -0,0 +1,30 @@
+/*
+    QUESTION ::
+        From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? 
+		What is the smallest number of wins for a team that did win the world series? Doing this will probably 
+		result in an unusually small number of wins for a world series champion – determine why this is the case. 
+		Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team 
+		with the most wins also won the world series? What percentage of the time?
+
+    SOURCES ::
+        * ...
+
+    DIMENSIONS ::
+        * ...
+
+    FACTS ::
+        * ...
+
+    FILTERS ::
+        * ...
+
+    DESCRIPTION ::
+        ...
+
+    ANSWER ::
+        ...
+
+*/
+
+SELECT *
+FROM ...;
