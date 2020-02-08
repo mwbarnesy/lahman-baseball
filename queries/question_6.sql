06534aab64e8858ade50502646ac3457df554d5b
mwbarnesy <mwbarnesy@gmail.com>
2020-02-07 23:03:21 -0600

    Answered Question 6! Changed the data types to numeric in order to divide sb by sb_attempts.

--- queries/question_6.sql
+++ queries/question_6.sql
@@ -18,36 +18,46 @@
         Year = 2016, players who ATTEMPTED to steal >= 20 
 
     DESCRIPTION ::
-        ...
+      	I started strong by filtering by year, and adding the stolen bases and caught stealing amounts together to
+		find the attempts, but I struggled to figure out how to put everything together to find the percentage. 
+		Finally I realized that I need to convert the data types in sb and cs to numeric before I could perform any
+		division.
 
     ANSWER ::
-        ...
+        Chris Owings had the most success stealing bases with a 91.3% success rate.
 
 */
 
 SELECT 
-	playerid, 
-	sb, 
+	p.namefirst || ' ' || p.namelast AS player_name, 
+	sb,
 	cs,
-	sb + cs AS sb_attemptsb --,
-	--sb / (sb + cs) AS success -- not working like I thought it would
-FROM batting 
+	sb + cs AS sb_attempts,
+	ROUND(CAST(sb AS numeric) / (CAST(sb AS numeric) + CAST(cs AS numeric)), 4) * 100 AS success
+FROM batting AS b
+	INNER JOIN people AS p
+	ON b.playerid = p.playerid
 WHERE yearid = 2016
 	AND sb + cs >= 20
-ORDER BY sb DESC
+GROUP BY player_name, sb, cs, sb_attempts, success
+ORDER BY success DESC
 ;
 
 
 
+-- Initial attempt --
+
 SELECT
 	p.namefirst || ' ' || p.namelast AS player_name, -- CONCAT shorthand with a space between ' '
-	SUM(sb) + SUM(cs) AS sb_attempts
+	SUM(sb) + SUM(cs) AS sb_attempts,
+	sb,
+	cs
 FROM batting AS b
 		INNER JOIN people AS p
 		ON b.playerid = p.playerid
 WHERE yearid = 2016
 	AND sb IS NOT NULL
 	AND cs IS NOT NULL
-GROUP BY p.namefirst, p.namelast
+GROUP BY p.namefirst, p.namelast, sb, cs
 ORDER BY sb_attempts DESC -- REMEMBER, you are looking for the best percentage, not the most attempts.
 ;
