06ae5d9d96b9fcf5cc6b63650e1856d9c61bdf0a
mwbarnesy <mwbarnesy@gmail.com>
2020-07-23 16:17:06 -0500

    initial commit

---
+++ queries/question_8.sql
@@ -0,0 +1,30 @@
+/*
+    QUESTION ::
+        Using the attendance figures from the homegames table, find the teams and parks which had the 
+		top 5 average attendance per game in 2016 (where average attendance is defined as total 
+		attendance divided by number of games). Only consider parks where there were at least 10 games 
+		played. Report the park name, team name, and average attendance. Repeat for the lowest 5 
+		average attendance.
+
+    SOURCES ::
+        * ...
+
+    DIMENSIONS ::
+        * ...
+
+    FACTS ::
+        * ...
+
+    FILTERS ::
+        * ...
+
+    DESCRIPTION ::
+        ...
+
+    ANSWER ::
+        ...
+
+*/
+
+SELECT *
+FROM ...;
