523dba00801e2488ebbb985244b584d67c3cab78
mwbarnesy <mwbarnesy@gmail.com>
2020-07-30 16:34:13 -0500

    Looking into making main query from teams table, and joining homegames, but there are still duplicate values. Not sure why yet.

--- queries/question_8.sql
+++ queries/question_8.sql
@@ -26,12 +26,36 @@
 
 */
 
+/* 
+:::::
+Here I am able to get the team and park foreign keys, 
+and order the data by the five higest average attendance. 
+:::::
+*/
+
 SELECT 
 	team, 
 	park,
 	attendance / games AS avg_attendance
 FROM homegames
-WHERE games > 10 AND year = 2016;
+WHERE games > 10 AND year = 2016
+ORDER BY avg_attendance DESC
+LIMIT 5;
+
+
+SELECT 
+	t.park,
+	t.name,
+	h.attendance / h.games AS avg_attendance
+FROM teams AS t
+LEFT JOIN homegames AS h
+ON t.teamid = h.team
+WHERE t.yearid = 2016 
+ORDER BY avg_attendance DESC;
+
+SELECT g, name, park, attendance
+FROM teams
+WHERE yearid = 2016;
 
 
 /* 
