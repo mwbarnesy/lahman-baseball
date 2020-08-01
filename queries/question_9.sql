1802a78589a536c5341d00b478767bdd1c56f12b
mwbarnesy <mwbarnesy@gmail.com>
2020-07-31 20:17:52 -0500

    Initial commit

---
+++ queries/question_9.sql
@@ -0,0 +1,28 @@
+/*
+    QUESTION ::
+        Which managers have won the TSN Manager of the Year award in both the National 
+		League (NL) and the American League (AL)? Give their full name and the teams that 
+		they were managing when they won the award.
+
+    SOURCES ::
+        * ...
+
+    DIMENSIONS ::
+        * ...
+
+    FACTS ::
+        * ...
+
+    FILTERS ::
+        * ...
+
+    DESCRIPTION ::
+        ...
+
+    ANSWER ::
+        ...
+
+*/
+
+SELECT *
+FROM ...;
